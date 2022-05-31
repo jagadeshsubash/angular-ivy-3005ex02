bec246af859f53ff3ce28d712896f81fb45d345b
Jagadesh <32300213+jagadeshsubash@users.noreply.github.com>
2022-05-31 12:37:39 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,5 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   name = 'Angular ' + VERSION.major  + " @ " + VERSION.minor;
   title =  "Geek or Freak";
-
-
+src = "https://www.google.com/search?q=images&prmd=ibnv&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi7-OmwlIn4AhVKwjgGHWU2BEQQ_AUoAXoECAIQAQ&biw=640&bih=280&dpr=2#imgrc=2nDXavJs9DoKTM";
 }
