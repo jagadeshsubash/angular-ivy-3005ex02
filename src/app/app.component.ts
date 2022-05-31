a267493ca534f25ad5ff52d4a34b795d4b080053
Jagadesh <32300213+jagadeshsubash@users.noreply.github.com>
2022-05-31 13:45:53 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ export class AppComponent  {
   name = 'Angular ' + VERSION.major  + " @ " + VERSION.minor;
   title =  "Geek or Freak";
 src = 'https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200-1.png';  
+bool = 'true';  
 }   
 
